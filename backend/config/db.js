a30e1ddedda9d5939a6fb577d02e5b4d7b97c5b6
Nilesh Pulate <156590937+N8758@users.noreply.github.com>
2026-05-03 13:25:57 +0530

    Update db.js

--- backend/config/db.js
+++ backend/config/db.js
@@ -2,14 +2,10 @@ require('dotenv').config();
 const { Pool } = require('pg');
 
 const pool = new Pool({
-  user: process.env.DB_USER,
-  host: process.env.DB_HOST,
-  database: process.env.DB_NAME,
-  password: process.env.DB_PASSWORD,
-  port: process.env.DB_PORT,
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false
+  }
 });
-pool.connect()
-  .then(() => console.log("DB Connected ✅"))
-  .catch(err => console.log(err));
 
 module.exports = pool;

a8e20119c374ee0d3b3eeaadf15736c9961d1bb7
Nilesh Pulate <156590937+N8758@users.noreply.github.com>
2026-05-03 13:35:01 +0530

    Update api.js

--- frontend/src/api/api.js
+++ frontend/src/api/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const API = axios.create({
-  baseURL: 'http://localhost:5000/api',
+  baseURL: 'https://role-based-task-manager-uinl.onrender.com/api',
 });
 
 API.interceptors.request.use(
